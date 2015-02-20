fcffef17c404f848080234503db17c40ed271711
bernardndegwa <mrbernardndegwa@gmail.com>
2015-02-20 18:36:47 +0300

    Refactored the functional tests

--- functional_tests.py
+++ functional_tests.py
@@ -8,6 +8,13 @@ class NewVisitorTestCase(unittest.TestCase):
     def setUp(self):
         self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(5)
+    def tearDown(self):
+        self.browser.quit()
+        
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         #Effie has heard about a new cool online to-do app. She goes 
@@ -32,21 +39,16 @@ class NewVisitorTestCase(unittest.TestCase):
         #When she hits enter the page updates, and now the page lists 
         #"1. Buy peacock feathers" as an item in a to do list
         inputbox.send_keys(Keys.ENTER)     
-        #self.assertIn('1: Buy peacock feathers', [row.text for row in rows])           
-        thetable = self.browser.find_element_by_id('id_list_table')
-        rows = thetable.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows]) 
-        '''self.assertTrue(any(row.text == '1: Buy peacock feathers' for row in rows),
-                        "New to-do item did not appear in table -- its text was:\n%s" %(
-                                                                                        table.text,
-                                                                                        )
-                        )'''
-
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
         #There is still a text box inviting her to add another item. 
         #She enters "Use peacock feathers to make a fly"
-        self.fail('Finish test')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
         #The page updates again, and now shows both items on her to
         #do list.
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
         #Edith wonders if the page will remember her to do list and
         #then notices that the site has generated a unique url for her
@@ -56,8 +58,7 @@ class NewVisitorTestCase(unittest.TestCase):
 
         #Satisfied she goes back to sleep
 	
-    def tearDown(self):
-        self.browser.quit()
+    
 
 if __name__ == '__main__':
         unittest.main()
