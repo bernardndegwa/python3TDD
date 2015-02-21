f47e13dfaed49e06d14695bd8360d38f32a6e01e
bernardndegwa <mrbernardndegwa@gmail.com>
2015-02-21 14:07:01 +0300

    Merging my branch with Harry's finally

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
 from lists.models import Item
-
+#merged with harry's on a branch called merging
 def home_page(request):
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
